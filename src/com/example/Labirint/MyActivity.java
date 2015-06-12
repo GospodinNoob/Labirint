c934ef99f14b2d8791df05a0ea2147f007c152ac
KapranvIvan <ivankapranov1998@gmail.com>
2015-06-12 10:45:54 +0300

    Create pages : Start, About, Settings; button Exit

--- src/com/example/Labirint/MyActivity.java
+++ src/com/example/Labirint/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.Labirint;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,19 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onClickToSettings1(View view) {
+        Intent intent = new Intent(MyActivity.this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickToAboutProgram(View view) {
+        Intent intent = new Intent(MyActivity.this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickExit(View view) {
+      finish();
     }
 }
---
+++ src/com/example/Labirint/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.Labirint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by home-pc on 12.06.2015.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.settings);
+    }
+}

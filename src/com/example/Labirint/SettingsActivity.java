e53c787024bbc796d0ace346ef0481a165e39426
KapranvIvan <ivankapranov1998@gmail.com>
2015-06-12 12:45:44 +0300

    Settings update

--- src/com/example/Labirint/SettingsActivity.java
+++ src/com/example/Labirint/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.Labirint;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by home-pc on 12.06.2015.
@@ -13,5 +15,50 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings);
+    }
+
+    public void onClickMonsters(View view) {
+
+    }
+
+    public void onClickMain(View view) {
+
+    }
+
+    public void onClickWalls(View view) {
+
+    }
+
+    public void onClickOtherObjects(View view) {
+
+    }
+
+    public void onClickOtherItems(View view) {
+
+    }
+
+    public void onClickWeapon(View view) {
+
+    }
+
+    public void onClickDoors(View view) {
+
+    }
+
+    public void onClickTeleports(View view) {
+
+    }
+
+    public void onClickRivers(View view) {
+
+    }
+
+    public void onClickLands(View view) {
+
+    }
+
+    public void onClickToGame(View view)
+    {
+
     }
 }

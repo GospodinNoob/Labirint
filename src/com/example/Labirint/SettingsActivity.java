256270b0bc2a64905eb01c8e3c3aeebcf0cd0a52
KapranvIvan <ivankapranov1998@gmail.com>
2015-06-13 13:43:58 +0300

    Настройки с отловом данных.

--- src/com/example/Labirint/SettingsActivity.java
+++ src/com/example/Labirint/SettingsActivity.java
@@ -4,14 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 /**
  * Created by home-pc on 12.06.2015.
  */
+
+
+
+
 public class SettingsActivity extends Activity {
+
+    public Switch switchTypeGame;
+    public Switch switchTypeExit;
+
+    public EditText countPlayers;
+    public EditText countChests;
+    public EditText sizeSquareLabirint;
+
+    public SeekBar densityWalls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        switchTypeGame = (Switch)findViewById(R.id.switch_type);
+        switchTypeExit = (Switch)findViewById(R.id.switch_exit);
+
+        countPlayers = (EditText)findViewById(R.id.edit_players_counter);
+        countChests = (EditText)findViewById(R.id.edit_count_chests);
+        sizeSquareLabirint = (EditText)findViewById(R.id.edit_size_labirint_square);
+
+        densityWalls = (SeekBar)findViewById(R.id.seekBar_walls);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings);

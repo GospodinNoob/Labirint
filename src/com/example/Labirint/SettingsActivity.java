9d60e1bb15e1435d3f3517dc75d068b275250cc7
KapranvIvan <ivankapranov1998@gmail.com>
2015-06-13 13:12:12 +0300

    Настройки для начала. Пока без передачи данных.

--- src/com/example/Labirint/SettingsActivity.java
+++ src/com/example/Labirint/SettingsActivity.java
@@ -17,45 +17,6 @@ public class SettingsActivity extends Activity {
         setContentView(R.layout.settings);
     }
 
-    public void onClickMonsters(View view) {
-
-    }
-
-    public void onClickMain(View view) {
-
-    }
-
-    public void onClickWalls(View view) {
-
-    }
-
-    public void onClickOtherObjects(View view) {
-
-    }
-
-    public void onClickOtherItems(View view) {
-
-    }
-
-    public void onClickWeapon(View view) {
-
-    }
-
-    public void onClickDoors(View view) {
-
-    }
-
-    public void onClickTeleports(View view) {
-
-    }
-
-    public void onClickRivers(View view) {
-
-    }
-
-    public void onClickLands(View view) {
-
-    }
 
     public void onClickToGame(View view)
     {
